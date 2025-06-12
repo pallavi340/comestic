154a489bb2b1b5bb7148f7e176a755d8a5e347bb
jagriti2004 <prakashjagriti32@gmail.com>
2025-06-12 14:00:44 +0530

    hello

--- resources/views/base/home.blade.php
+++ resources/views/base/home.blade.php
@@ -8,21 +8,21 @@
 
       <div class="col-md-4">
         <div class="card promo-card h-100">
-          <img src="https://i.imgur.com/X0MyWhR.png" class="card-img-top promo-img" alt="Nykaa Wanderlust Offer">
+          <img src="https://th.bing.com/th/id/OIP.XjRkG5dLWLiXlcZi0DEHEQAAAA?rs=1&pid=ImgDetMain" class="card-img-top promo-img" alt="Nykaa Wanderlust Offer">
         </div>
       </div>
 
 
       <div class="col-md-4">
         <div class="card promo-card h-100">
-          <img src="https://i.imgur.com/KYg8khn.png" class="card-img-top promo-img" alt="Charlotte Tilbury Offer">
+          <img src="https://th.bing.com/th/id/OIP.cy9hG0ufbf4YTORUXRFwSAHaHa?w=1000&h=1000&rs=1&pid=ImgDetMain" class="card-img-top promo-img" alt="Charlotte Tilbury Offer">
         </div>
       </div>
 
    
       <div class="col-md-4">
         <div class="card promo-card h-100">
-          <img src="https://i.imgur.com/9OKX0so.png" class="card-img-top promo-img" alt="Dot & Key Offer">
+          <img src="https://media6.ppl-media.com/tr:h-750,w-750,c-at_max,dpr-2/static/img/product/342495/dot-and-key-vitamin-c-e-skin-care-gift-set-for-glowing-skin_1_display_1672649517_70c75764.jpg" class="card-img-top promo-img" alt="Dot & Key Offer">
         </div>
       </div>
 
