7d761a9aad5af2209dc40567ec92347a3f661e73
Pallavi Rani <pallavicourt@gmail.com>
2025-06-14 20:48:05 +0530

    2commit

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,13 @@ use App\Http\Controllers\GoogleController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', [HomeController::class, 'home'])->name('base.home');
 Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+
+Route::get('/google/redirect', [GoogleController::class, 'redirectToGoogle'])->name('google.redirect');
+Route::get('/google/callback', [GoogleController::class, 'handleGoogleCallback'])->name('google.callback');
+Route::get('/login', [GoogleController::class, 'login'])->name('base.login');
+Route::resource('/category', CategoryController::class);
+
